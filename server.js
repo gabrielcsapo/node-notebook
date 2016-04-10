ac309b06b9c530d151f9a0a48c56d990ec21a71e
Gabriel Csapo <gabecsapo@gmail.com>
2016-04-09 18:29:52 -0700

    adds tests for sync and async code

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false, verify:function(req,res,buf){req.rawBody=buf}}));
 app.use(bodyParser.json({verify:function(req,res,buf){req.rawBody=buf}}));
 
-
 app.post('/api/run', function(req, res) {
-  var type = req.body.type;
+  var type = req.body.type || 'sync';
   var script = req.body.script || req.rawBody.toString('utf8').replace('script=', '');
   vm.run(script, type, undefined, function(result) {
     res.send(result);
@@ -19,3 +18,5 @@ app.post('/api/run', function(req, res) {
 app.listen(port, function() {
   console.log('listening on localhost:', port); // eslint-disable-line no-console
 });
+
+module.exports = app;
---
+++ test/server.js
@@ -0,0 +1,46 @@
+var request = require('supertest');
+var app = require('../server');
+var assert = require('chai').assert;
+
+describe('Sever', function() {
+
+  it('should test /api/run with sync code', function(done) {
+    this.timeout(50000);
+    var code = {'script': 'var moment=require("moment");moment().format("x");'};
+    request(app)
+      .post('/api/run')
+      .type('json')
+      .send(code)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) { throw err; }
+        assert.isObject(res.body, 'response is an object');
+        assert.isArray(res.body.trace, 'response trace is an Array');
+        assert.isString(res.body.result, 'response result is a string');
+        assert.equal(res.body.type, 'String', 'response type is string');
+        done();
+      });
+  });
+
+  it('should test /api/run with async code', function(done) {
+    this.timeout(50000);
+    var code = {'type': 'async', 'script': 'var request = require("request-promise"); var response = await request({ url: "https://graph.facebook.com/?id=http://news.ycombinator.com", json: true });'};
+    request(app)
+      .post('/api/run')
+      .type('json')
+      .send(code)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) { throw err; }
+        assert.isObject(res.body, 'response is an object');
+        assert.isArray(res.body.trace, 'response trace is an Array');
+        assert.isObject(res.body.result, 'response result is a Object');
+        assert.equal(res.body.result.id, 'http://news.ycombinator.com', 'id is http://news.ycombinator.com');
+        assert.isNumber(res.body.result.shares, 'shares is a number');
+        assert.isNumber(res.body.result.comments, 'comments is a number');
+        assert.equal(res.body.type, 'Object', 'response type is Object');
+        done();
+      });
+  });
+
+});
